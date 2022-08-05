a78a46eb3c6fe0c26b9b73f16a14cc3bd388a6e1
SLeBron <sophieclebron@gmail.com>
2022-08-04 21:55:36 -0600

    began editing starter code

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = Math.floor(Math.random() * 128) + 7;
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var userChoice;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +16,14 @@ function writePassword() {
 
 }
 
+function generatePassword(length, includeLower, includeUpper, includeNum, includeSpecial){
+
+  // Returns a random integer from 0 to 10:
+  // Math.floor(Math.random() * 11);
+
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
